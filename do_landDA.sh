3373c5270780f21b7b7dc0ceb8c3c3390df9c7ff
Tseganeh Gichamo <1621305073113305@mil>
2024-11-08 06:55:11 -0500

    fix restart dir

--- do_landDA.sh
+++ do_landDA.sh
@@ -130,31 +130,28 @@ DP=`echo $PREVDATE | cut -c7-8`
 HP=`echo $PREVDATE | cut -c9-10`
 
 FILEDATE=${YYYY}${MM}${DD}.${HH}0000
-if  [[ $SAVE_TILE == "YES" ]]; then
-    for tile in 1 2 3 4 5 6 
+
+RSTRDIR=${WORKDIR}
+
+if  [[ $SAVE_TILE == "YES" ]]; then   
+    for ie in $(seq $ensemble_size)
     do
-    cp ${RSTRDIR}/${FILEDATE}.sfc_data.tile${tile}.nc  ${RSTRDIR}/${FILEDATE}.sfc_data_back.tile${tile}.nc
-    done
-    if [[ "$ensemble_size" -gt 1  ]]; then 
-        for ie in $(seq $ensemble_size)
-        do
-            mem_ens="mem`printf %03i $ie`"     #RSTRDIR=${MEM_WORKDIR}
-            for tile in 1 2 3 4 5 6 
-            do 
-            cp ${RSTRDIR}/${mem_ens}/${FILEDATE}.sfc_data.tile${tile}.nc  ${RSTRDIR}/${mem_ens}/${FILEDATE}.sfc_data_back.tile${tile}.nc
-            done    
-        done  
-    fi
+        if [[ "$ensemble_size" -eq 1  ]]; then 
+            mem_ens="mem000"   
+        else
+            mem_ens="mem`printf %03i $ie`"     
+        fi
+        for tile in 1 2 3 4 5 6 
+        do 
+        cp ${RSTRDIR}/${mem_ens}/${FILEDATE}.sfc_data.tile${tile}.nc  ${RSTRDIR}/${mem_ens}/${FILEDATE}.sfc_data_back.tile${tile}.nc
+        done    
+    done  
 fi 
 
 #stage restarts for applying JEDI update (files will get directly updated)
-for tile in 1 2 3 4 5 6 
-do
-  ln -fs ${RSTRDIR}/${FILEDATE}.sfc_data.tile${tile}.nc ${JEDIWORKDIR}/restarts/${FILEDATE}.sfc_data.tile${tile}.nc
-done
+
 cres_file=${JEDIWORKDIR}/restarts/${FILEDATE}.coupler.res
-if [[ -e  ${RSTRDIR}/${FILEDATE}.coupler.res ]]; then 
-    # ln -sf ${RSTRDIR}/${FILEDATE}.coupler.res $cres_file
+if [[ -e  ${RSTRDIR}/mem000/${FILEDATE}.coupler.res ]]; then 
     cp ${RSTRDIR}/${FILEDATE}.coupler.res $cres_file
 else #  if not present, need to create coupler.res for JEDI 
     cp ${LANDDADIR}/template.coupler.res $cres_file
@@ -170,7 +167,13 @@ else #  if not present, need to create coupler.res for JEDI
     sed -i -e "s/XXHP/${HP}/g" $cres_file
 
 fi 
-if [[ "$ensemble_size" -gt 1  ]]; then
+if [[ "$ensemble_size" -eq 1  ]]; then
+    mem_ens="mem000"
+    for tile in 1 2 3 4 5 6 
+    do
+        ln -fs ${RSTRDIR}/${mem_ens}/${FILEDATE}.sfc_data.tile${tile}.nc ${JEDIWORKDIR}/restarts/${FILEDATE}.sfc_data.tile${tile}.nc
+    done
+else    
     for ie in $(seq $ensemble_size)
     do
         mem_ens="mem`printf %03i $ie`"
