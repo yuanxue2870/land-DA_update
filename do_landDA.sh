9386e76b5bfc85bbb6f334cfe92bf2702a0836f5
yuanxue2870 <yuan.xue@noaa.gov>
2024-11-15 16:13:37 +0000

    Change to Yuan's method to calculate HB

--- do_landDA.sh
+++ do_landDA.sh
@@ -95,15 +95,13 @@ MM=`echo $THISDATE | cut -c5-6`
 DD=`echo $THISDATE | cut -c7-8`
 HH=`echo $THISDATE | cut -c9-10`
 
-DABEGIN=`${INCDATE} $THISDATE -$HALFWINLEN`
-HB=`echo $DABEGIN | cut -c9-10`
-
 PREVDATE=`${INCDATE} $THISDATE -$WINLEN`
 
 YYYP=`echo $PREVDATE | cut -c1-4`
 MP=`echo $PREVDATE | cut -c5-6`
 DP=`echo $PREVDATE | cut -c7-8`
 HP=`echo $PREVDATE | cut -c9-10`
+HB=$(printf %02d $(( $HP+$WINLEN/2))
 
 FILEDATE=${YYYY}${MM}${DD}.${HH}0000
 
